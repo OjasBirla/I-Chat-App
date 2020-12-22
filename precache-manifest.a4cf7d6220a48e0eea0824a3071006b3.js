64e7701e4e5141618e9f12e20bc6beabb9d3b403
OjasBirla <68767697+OjasBirla@users.noreply.github.com>
2020-12-22 12:45:22 +0530

    Add files via upload

---
+++ precache-manifest.a4cf7d6220a48e0eea0824a3071006b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15021ef91079141d39a0e846ffdc9990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc238a5d4dd1f4e8c4b6",
+    "url": "/static/css/main.291f61c0.chunk.css"
+  },
+  {
+    "revision": "ac14dc6d8d40464165c4",
+    "url": "/static/js/2.f43bbd64.chunk.js"
+  },
+  {
+    "revision": "dc238a5d4dd1f4e8c4b6",
+    "url": "/static/js/main.3b888013.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  },
+  {
+    "revision": "20faec29c9db49d5f1b7b8af6fc39bbf",
+    "url": "/static/media/bg.20faec29.jpg"
+  }
+]);
